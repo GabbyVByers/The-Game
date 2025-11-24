ff0fa25f9e456f363445c9cd256dd32906bc031d
Gabby <GabbyVByers@gmail.com>
2025-11-24 12:16:19 -0800

    SFML Installed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 
-#include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
-int main() {
-	std::cout << "Calc is short for calculator btw\n";
-	return 0;
+int main()
+{
+    sf::RenderWindow window;
+    window.create(sf::VideoMode({ 800, 800 }), "My Window");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Blue);
+        window.display();
+    }
+
+    return 0;
 }
 
